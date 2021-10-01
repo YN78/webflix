1c52dab28b682a26d8cf1793f9864c8c19f01d93
YN78 <72245124+YN78@users.noreply.github.com>
2021-10-01 22:37:52 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<div class="header" id="topheader">
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
+    <div class="container text-uppercase p-2">
+  <a class="navbar-brand font-weight-bold text-white" href=""> <img src="images/logo.png" width="40%" ></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item active">
+        <a class="nav-link"  href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#servicediv">TV Shows</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#pricingdiv">Movies</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#project-work"><i class="fas fa-search"></i></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="profile.php"><i class="fas fa-user"></i></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#contactid"><i class="fas fa-sign-out-alt"></i></a>
+      </li>
+    </ul>
+  </div>
+ </div>
+ </nav>
+
+
+
+ 
+
+
+<!--********************header part end*****************-->
